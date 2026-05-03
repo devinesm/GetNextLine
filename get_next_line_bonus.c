80e8eada9f9c7a706d14bc7048a4982007c8e5c2
devinesm <inesmonteirosara@gmail.com>
2026-05-03 02:24:15 +0100

    add bonus part

---
+++ get_next_line_bonus.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipinto-m <ipinto-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/03 01:18:53 by ipinto-m          #+#    #+#             */
+/*   Updated: 2026/05/03 02:21:40 by ipinto-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*read_file(int fd, char *result)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (free(result), NULL);
+	bytes_read = 1;
+	while (bytes_read != -1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(buffer), free(result), NULL);
+		if (bytes_read == 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		result = ft_strjoin_free(result, buffer);
+		if (!result)
+			return (free(buffer), NULL);
+		if (ft_strchr(result, '\n'))
+			break ;
+	}
+	free(buffer);
+	return (result);
+}
+
+static char	*ft_new_line(char *s)
+{
+	char	*line;
+	size_t	i;
+
+	if (!s || !s[0])
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	line = malloc(i + (s[i] == '\n') + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*ft_update_stash(char *buffer)
+{
+	char	*new_buffer;
+	size_t	i;
+	size_t	j;
+
+	if (!buffer)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i] || !buffer[i + 1])
+		return (free(buffer), NULL);
+	new_buffer = malloc(ft_strlen(buffer + i + 1) + 1);
+	if (!new_buffer)
+		return (free(buffer), NULL);
+	i++;
+	j = 0;
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash[4096];
+	char		*line;
+
+	if (fd < 0 || fd > 4096 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stash[fd] = read_file(fd, stash[fd]);
+	if (!stash[fd])
+		return (NULL);
+	line = ft_new_line(stash[fd]);
+	if (!line)
+	{
+		free(stash[fd]);
+		stash[fd] = NULL;
+		return (NULL);
+	}
+	stash[fd] = ft_update_stash(stash[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipinto-m <ipinto-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/03 01:48:17 by ipinto-m          #+#    #+#             */
+/*   Updated: 2026/05/03 02:19:30 by ipinto-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin_free(char *s1, const char *s2);
+
+#endif
